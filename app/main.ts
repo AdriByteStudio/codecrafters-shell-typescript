a904b15c483c471fee255d24e7feaa07d96c87b3
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-22 01:52:37 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -10,6 +10,10 @@ rl.prompt();
 
 rl.on("line", (input: string) => {
   const command = input.trim();
+  if (command === "exit") {
+    rl.close();
+    return;
+  }
   console.log(`${command}: command not found`);
   rl.prompt();
 });
